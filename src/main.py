17bc6d6d5cb424c7804993698501cc437f6f1f30
Eric Melz <eric@emelz.com>
2025-04-06 17:56:50 -0700

    work on getting weight

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from urllib.parse import urlencode
 
 import httpx
 from fastapi import FastAPI, Request
+from fastapi import HTTPException
 from fastapi.responses import RedirectResponse, JSONResponse
 from pydantic_settings import BaseSettings
 
@@ -13,6 +14,8 @@ SESSIONS = {}
 REDIRECT_URI = "http://perfin.ai:9876/withings-callback"
 AUTH_URL = "https://account.withings.com/oauth2_user/authorize2"
 TOKEN_URL = "https://wbsapi.withings.net/v2/oauth2"
+
+KG_TO_LBS_MULTIPLIER = 2.20462
 
 
 class Settings(BaseSettings):
@@ -62,15 +65,38 @@ async def callback(request: Request):
 
 @app.get("/weight")
 async def get_weight(session_id: str):
-    access_token = SESSIONS.get(session_id)
+    # access_token = SESSIONS.get(session_id)
+    access_token = '871600de2536e68d71bd4d6dd26084566b509a98'
+    print(f"access_token={access_token}")
     if not access_token:
         return JSONResponse({"error": "unauthorized"}, status_code=401)
 
     async with httpx.AsyncClient() as client:
-        resp = await client.post("https://wbsapi.withings.net/measure", data={
-            "action": "getmeas",
-            "meastype": 1,
-            "category": 1,
-            "access_token": access_token,
-        })
-    return resp.json()
+        try:
+            resp = await client.post("https://wbsapi.withings.net/measure", data={
+                "action": "getmeas",
+                "meastype": 1,
+                "category": 1,
+                "access_token": access_token,
+            })
+            if resp.status_code == 200:
+                data = resp.json()
+                weights = [
+                    (group["date"], meas["value"] * 10 ** meas["unit"] * KG_TO_LBS_MULTIPLIER)
+                    for group in data["body"]["measuregrps"]
+                    for meas in group["measures"]
+                    if meas["type"] == 1
+                ]
+                return JSONResponse(content=weights)
+            else:
+                print(f'Raising exception {resp.status_code}')
+                raise HTTPException(
+                    status_code=502,
+                    detail=f"Failed to fetch Withings data.  Status: {resp.status_code}"
+                )
+        except httpx.RequestError as e:
+            print(f'Raising exception:: {e}')
+            raise HTTPException(
+                status_code=503,
+                detail=f"Network error when contacting Withings: {str(e)}"
+            )
